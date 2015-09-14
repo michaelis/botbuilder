4e5873865b46aa8408653bb4f0da0881f7d23628
Miguel Gomes <miguelgomes@MacBook-Pro-de-Miguel.local>
2015-09-14 10:36:03 +0100

    Initial Commit

---
+++ CodebitsApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CodebitsApp
+//
+//  Created by Miguel Gomes on 14/09/15.
+//  Copyright (c) 2015 Miguel Gomes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CodebitsApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CodebitsApp
+//
+//  Created by Miguel Gomes on 14/09/15.
+//  Copyright (c) 2015 Miguel Gomes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
