63c9c466f6596331497dd7d1c015d3f24997f96e
Miguel Gomes <miguelgomes@MacBook-Pro-de-Miguel.local>
2015-09-21 16:16:14 +0100

    Final V3

---
+++ CodebitsApp/LoginTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginTableViewController.h
+//  CodebitsApp
+//
+//  Created by Miguel Gomes on 18/09/15.
+//  Copyright (c) 2015 Miguel Gomes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginTableViewController : UITableViewController
+
+@end
